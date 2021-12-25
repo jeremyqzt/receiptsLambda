cdeb3fd1fd98aba07368275c0c50c5d8bcff6bdc
Jeremy Qian <jq357@hotmail.com>
2021-12-25 16:09:49 -0500

    kinda works now

---
+++ loader.py
@@ -0,0 +1,19 @@
+import datetime
+import uuid
+
+class Loader:
+    def __init__(self):
+        ...
+
+    @staticmethod
+    def parse_data(json_dict):
+        user_id = json_dict.get("user_id", None)
+        business_id = json_dict.get("business_id", None)
+
+        return {
+            "id": str(uuid.uuid4()),
+            "user_id": user_id,
+            "business_id": business_id,
+            "created_at": datetime.datetime.now().isoformat()
+        }
+
--- receipts-api-handler.py
+++ receipts-api-handler.py
@@ -10,16 +10,15 @@ import datetime
 import dynamo
 import os
 from PIL import Image
-
-
+from loader import Loader
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 dynamodb = boto3.client('dynamodb')
 s3_client = boto3.client('s3')
 
-dest_bucket = 'image-bucket'
-table_name = 'attachments'
+dest_bucket = 'receipt-attach'
+table_name = 'attach-store'
 
 
 def create(event, context):
@@ -36,7 +35,7 @@ def create(event, context):
     lambda_path = "/tmp/" + file_name
     s3_path = "output/" + file_name
     os.system('echo testing... >'+lambda_path)
-    s3_client.meta.client.upload_file(lambda_path, dest_bucket, file_name)
+    s3_client.upload_file(lambda_path, dest_bucket, file_name)
 
     post_str = event['body']
     post = json.loads(post_str)
@@ -67,22 +66,18 @@ def get(event, context):
         "body": "An error occured while creating post."
     }
 
-    some_text = "test"
     file_name = "my_test_file.csv"
     lambda_path = "/tmp/" + file_name
     s3_path = "output/" + file_name
     os.system('echo testing... >'+lambda_path)
-    s3_client.meta.client.upload_file(lambda_path, dest_bucket, file_name)
+    s3_client.upload_file(lambda_path, dest_bucket, file_name)
 
-    post_str = event['body']
-    post = json.loads(post_str)
-    current_timestamp = datetime.datetime.now().isoformat()
-    post['createdAt'] = current_timestamp
+    post_str = Loader.parse_data(event)
 
-    s3 = boto3.client('s3')
+    logger.info(f'Store values is: {str(post_str)}')
 
     res = dynamodb.put_item(
-        TableName=table_name, Item=dynamo.to_item(post))
+        TableName=table_name, Item=dynamo.to_item(post_str))
 
     # If creation is successful
     if res['ResponseMetadata']['HTTPStatusCode'] == 200:
@@ -93,85 +88,3 @@ def get(event, context):
 
     return response
 
-
-
-def all(event, context):
-    # Set the default error response
-    response = {
-        "statusCode": 500,
-        "body": "An error occured while getting all posts."
-    }
-
-    scan_result = dynamodb.scan(TableName=table_name)['Items']
-
-    posts = []
-
-    for item in scan_result:
-        posts.append(dynamo.to_dict(item))
-
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(posts)
-    }
-
-    return response
-
-
-def update(event, context):
-    logger.info(f'Incoming request is: {event}')
-
-    post_id = event['pathParameters']['postId']
-
-    # Set the default error response
-    response = {
-        "statusCode": 500,
-        "body": f"An error occured while updating post {post_id}"
-    }
-
-    post_str = event['body']
-
-    post = json.loads(post_str)
-
-    res = dynamodb.update_item(
-        TableName=table_name,
-        Key={
-            'id': {'N': post_id}
-        },
-        UpdateExpression="set content=:c, author=:a, updatedAt=:u",
-        ExpressionAttributeValues={
-            ':c': dynamo.to_item(post['content']),
-            ':a': dynamo.to_item(post['author']),
-            ':u': dynamo.to_item(datetime.datetime.now().isoformat())
-        },
-        ReturnValues="UPDATED_NEW"
-    )
-
-    # If updation is successful for post
-    if res['ResponseMetadata']['HTTPStatusCode'] == 200:
-        response = {
-            "statusCode": 200,
-        }
-
-    return response
-
-
-def delete(event, context):
-    logger.info(f'Incoming request is: {event}')
-
-    post_id = event['pathParameters']['postId']
-
-    # Set the default error response
-    response = {
-        "statusCode": 500,
-        "body": f"An error occured while deleting post {post_id}"
-    }
-
-    res = dynamodb.delete_item(TableName=table_name, Key={
-                               'id': {'N': post_id}})
-
-    # If deletion is successful for post
-    if res['ResponseMetadata']['HTTPStatusCode'] == 200:
-        response = {
-            "statusCode": 204,
-        }
-    return response
